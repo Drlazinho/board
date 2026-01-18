931f06563fdd9fe40889e1a8725918730b4fa241
Lázaro Pimentel <142930247+LazaroBonfim@users.noreply.github.com>
2026-01-17 21:02:37 -0300

    Form Comentário

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,41 +1,47 @@
-import { Button } from '@/components/button'
-import { getIssue } from "@/http/get-issue"
-import { ArchiveIcon, MoveLeftIcon, ThumbsUpIcon } from 'lucide-react'
-import type { Metadata } from "next"
-import Link from 'next/link'
-import { IssueCommentsList } from './issue-comments/issue-comments.list'
-import { Suspense } from 'react'
-import { IssueCommentsSkeleton } from './issue-comments/issue-comments-skeleton'
+import { Button } from "@/components/button";
+import { getIssue } from "@/http/get-issue";
+import {
+  ArchiveIcon,
+  MessageCirclePlusIcon,
+  MoveLeftIcon,
+  ThumbsUpIcon,
+} from "lucide-react";
+import type { Metadata } from "next";
+import Link from "next/link";
+import { IssueCommentsList } from "./issue-comments/issue-comments.list";
+import { Suspense } from "react";
+import { IssueCommentsSkeleton } from "./issue-comments/issue-comments-skeleton";
+import { Input } from "@/components/input";
 
 interface IssuePageProps {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }
 
 export const generateMetadata = async ({
   params,
 }: IssuePageProps): Promise<Metadata> => {
-  const { id } = await params
+  const { id } = await params;
 
-  const issue = await getIssue({ id })
+  const issue = await getIssue({ id });
 
   return {
     title: `Issue ${issue.title}`,
-  }
-}
+  };
+};
 
 const statusLabels = {
   backlog: "Backlog",
   todo: "To Do",
   in_progress: "In Progress",
   done: "Done",
-} as const
+} as const;
 
 export default async function IssuePage({ params }: IssuePageProps) {
-  const { id } = await params
+  const { id } = await params;
 
-  const issue = await getIssue({ id })
+  const issue = await getIssue({ id });
 
-return (
+  return (
     <main className="max-w-[900px] mx-auto w-full flex flex-col gap-4 p-6 bg-navy-800 border-[0.5px] border-navy-500 rounded-xl">
       <Link
         href="/"
@@ -64,15 +70,28 @@ return (
         </p>
       </div>
 
-            <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2">
         <span className="font-semibold">Comments</span>
 
-        <form />
-
+        <form className="relative w-full">
+          <Input
+            className="bg-navy-700 h-11 pr-24 w-full"
+            placeholder="Leave a comment..."
+          />
+          <button
+            type="submit"
+            className="flex items-center gap-2 text-indigo-400 absolute right-3 top-1/2 -translate-y-1/2 text-xs hover:text-indigo-300 cursor-pointer disabled:opacity-50"
+          >
+            Publish
+            <MessageCirclePlusIcon className="size-3" />
+          </button>
+        </form>
         <div className="mt-3">
           <Suspense fallback={<IssueCommentsSkeleton />}>
             <IssueCommentsList issueId={issue.id} />
-          </Suspense>        </div>
+          </Suspense>{" "}
+        </div>
       </div>
     </main>
-  )}
+  );
+}
